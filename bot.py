5e592cda5e4514b6234ae243e247a8a4e2efc470
Vdganh <75907868+vdganh@users.noreply.github.com>
2022-10-28 14:53:17 -0700

    Add files via upload

---
+++ bot.py
@@ -0,0 +1,37 @@
+import json
+x = open('D:\VSC\CODINGAN\PYTHON\BOT WA\data.json')
+data = json.load(x)
+
+print('''
+1. Melatih Bot
+2. Berbicara dengan Bot
+''')
+
+
+while True:
+    input_pertama = input("Masukkan Kode: ")
+    if input_pertama == "1":
+        while True:
+            a = input("User\t: ")
+            if a == "Keluar":
+                break
+            else:
+                b = input("Bot\t: ")
+                data[a] = b
+                b = open('D:\VSC\CODINGAN\PYTHON\BOT WA\data.json', "w")
+                json.dump(data,b)
+                b.close()
+
+
+    elif input_pertama == "2":
+        while True:
+            a = input("User\t: ")
+            if a == 'Keluar':
+                break
+            if a in data:
+                print(f'Bot\t: {data[a]}')
+            else:
+                print('Bot\t: Itu kata baru')
+
+    else:
+        pass
